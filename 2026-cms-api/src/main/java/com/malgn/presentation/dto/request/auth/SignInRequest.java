187ae32736e75660ccd4bd8bc31f3b5c45978fe5
rudcjs <rudcjs335@gmail.com>
2026-03-29 06:53:44 +0900

    feat: SignInRequest Validation 추가 - username max = 50 - password max = 255

--- 2026-cms-api/src/main/java/com/malgn/presentation/dto/request/auth/SignInRequest.java
+++ 2026-cms-api/src/main/java/com/malgn/presentation/dto/request/auth/SignInRequest.java
@@ -2,13 +2,16 @@ package com.malgn.presentation.dto.request.auth;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record SignInRequest(
         @Schema(description = "로그인 아이디", example = "testuser", requiredMode = Schema.RequiredMode.REQUIRED)
         @NotBlank(message = "아이디를 입력해주세요.")
+        @Size(max = 50, message = "아이디는 최대 100자까지 입력 가능합니다.")
         String username,
         @Schema(description = "비밀번호", example = "passw0rd", requiredMode = Schema.RequiredMode.REQUIRED)
         @NotBlank(message = "비밀번호를 입력해주세요.")
+        @Size(max = 255, message = "비밀번호는 최대 255자까지 입력 가능합니다.")
         String password
 ) {
 }
